3d8440daef39355ca8e6ca35cf37c0a2e8e07613
Barret J. Nobel <bear.nobel@gmail.com>
2016-06-11 16:28:31 -0400

    added some buttons and labels for the cells

---
+++ BitList/AddToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddToDoTableViewCell.swift
+//  BitList
+//
+//  Created by Mr. Nobel on 6/11/16.
+//  Copyright © 2016 Mr. Nobel. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ BitList/ToDoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoTableViewCell.swift
+//  BitList
+//
+//  Created by Mr. Nobel on 6/11/16.
+//  Copyright © 2016 Mr. Nobel. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
